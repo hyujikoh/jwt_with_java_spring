a6618c76d19c786a6594684e731c024cba61b963
hyujikoh <dvum0045@gmail.com>
2024-05-18 18:01:57 +0900

    init commit

---
+++ src/test/java/com/example/jwtwithspring/JwtWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtWithSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
